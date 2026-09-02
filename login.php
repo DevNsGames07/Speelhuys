4d16f68d899e6359f1805eb8333003a2ed27ff19
NsGames07 <106678739+DevNsGames07@users.noreply.github.com>
2026-09-02 11:44:13 +0200

    Login werken + een basis overzicht pagina toegevoegd

--- login.php
+++ login.php
@@ -1,3 +1,27 @@
+<?php
+    $errorbericht = "";
+    if(!empty($_POST["username"])) {
+        include "./Classes/gebruiker.php";
+        include "./Classes/database.php";
+
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        if (empty($username) || empty($password)) {
+            $errorbericht = "Een van de velden is niet ingevuld.";
+        } else {
+            $gebruiker = Gebruiker::vindtGebruiker($username, $password);
+
+            if ($gebruiker) {
+                header("Location: overzichtadmin.php");
+                exit;
+            } else {
+                $errorbericht = "Geen geldige gebruiker";
+            }
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,14 +31,15 @@
 
 <h2>Login</h2>
 
-<form method="post" action="create.php">
+<form method="post" action="login.php">
     Gebruikersnaam: <br>
     <input type="text" name="username" required><br><br>
-
     Wachtwoord: <br>
     <input type="password" name="password" required><br><br>
-
     <input type="submit" value="Login">
+    <?php
+    echo "<p>" . $errorbericht . "</p>" // errorbericht
+    ?>
 </form>
 
 </body>
