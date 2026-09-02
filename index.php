ed99f21f6e81549d9212ba7ff91562f9eaad9d48
NsGames07 <106678739+DevNsGames07@users.noreply.github.com>
2026-09-02 11:53:30 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+    <?php
+    include "./Classes/database.php";
+    include "./Classes/sets.php";
+    $sets = Sets::AlleSets();
+    foreach ($sets as $set) {
+    }
+    ?>
+    <?php
+    //Thema per set
+    ?>
