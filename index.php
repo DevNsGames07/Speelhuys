8b72064fa4a2dc41437d54a786ac84318dd7522d
TumBungus1973 <stinkyfranky2020@gmail.com>
2026-09-02 14:24:23 +0200

    Broken ding gefixt

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
     include "./Classes/database.php";
     include "./Classes/sets.php";
     $sets = Sets::AlleSets();
-    foreach ($sets as $set) {
-    ?>
-        //Thema per set.
-    <?php
-    }
     ?>
 
     <!doctype html>
@@ -21,50 +16,37 @@
     </head>
 
     <body>
-        <nav>
+        <nav class="navbar navbar-expand-lg" style="background-color: #1bb19b;" data-bs-theme="light">
             <div class="container-fluid">
-                <span class="text-white fw-bold">Speelhuys</span>
-                <a href="./admin/index.php">log in</a>
+                <span class="navbar-brand" href="">Speelhuys </br> codeblokken</span>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <a class="nav-link" href="../speelhuys/admin/index.php">Log in</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" aria-disabled="true">Winkelwagen</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
-        <div class="container">
-            <div class="card">
-
-            </div>
-
-            <body>
-                <nav class="navbar navbar-expand-lg" style="background-color: #1bb19b;" data-bs-theme="light">
-                    <div class="container-fluid">
-                        <span class="navbar-brand" href="">Speelhuys </br> codeblokken</span>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <a class="nav-link" href="../speelhuys/admin/index.php">Log in</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link disabled" aria-disabled="true">Winkelwagen</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <div class="container mt-4">
-                    <div class="row">
-                        <!-- loopt door bericht en toont ze als cards -->
-                        <?php foreach ($sets as $set) { ?>
-                            <div class="col-md-4 mb-4">
-                                <a href="detail.php?id=<?= $set->id; ?>" class="text-decoration-none text-dark">
-                                    <div class="card">
-                                        <img src="images/sets/<?= $set->image; ?>" class="card-img-top" alt="<?= $set->naam; ?>" style="height: 180px; object-fit: cover;">
-                                        <div class="card-body">
-                                            <h5 class="card-title"><?= $set->naam; ?></h5>
-                                            <p class="card-text text-muted"><?= $set->stukjes; ?></p>
-                                        </div>
-                                    </div>
-                                </a>
+        <div class="container mt-4">
+            <div class="row">
+                <!-- loopt door bericht en toont ze als cards -->
+                <?php foreach ($sets as $set) { ?>
+                    <div class="col-md-4 mb-4">
+                        <a href="detail.php?id=<?= $set->id; ?>" class="text-decoration-none text-dark">
+                            <div class="card">
+                                <img src="images/sets/<?= $set->image; ?>" class="card-img-top" alt="<?= $set->naam; ?>" style="height: 180px; object-fit: cover;">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?= $set->naam; ?></h5>
+                                    <p class="card-text text-muted"><?= $set->stukjes; ?></p>
+                                </div>
                             </div>
-                        <?php } ?>
+                        </a>
                     </div>
-                </div>
-            </body>
+                <?php } ?>
+            </div>
         </div>
     </body>
+    </div>
