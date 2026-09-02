0c5955d6b9c04291a2df2032c97329775cc6787f
AgentAlex2004 <194211@novacollege.nl>
2026-09-02 12:22:22 +0200

    update login.php

--- login.php
+++ login.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<h2>Login</h2>
-
-<form method="post" action="create.php">
-    Gebruikersnaam: <br>
-    <input type="text" name="username" required><br><br>
-
-    Wachtwoord: <br>
-    <input type="password" name="password" required><br><br>
-
-    <input type="submit" value="Login">
-</form>
-
+<div class="login-container">
+    <div class="login-box">
+        <h2>Inloggen:</h2>
+        <form method="post" action="create.php">
+            <div class="input-box">
+                <input type="text" name="username" required>
+            </div>
+            <div class="input-box">
+                <input type="password" name="password" required>
+            </div>
+            <input type="submit" value="Continue" class="login-button">
+        </form>
+    </div>
+</div>
+</div>
+</header>
 </body>
 </html>
