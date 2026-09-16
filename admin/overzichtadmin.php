9c8e763dce4ef6cb49a1358408abb855a0adc254
TumBungus1973 <stinkyfranky2020@gmail.com>
2026-09-16 14:14:07 +0200

    design update

--- admin/overzichtadmin.php
+++ admin/overzichtadmin.php
@@ -53,21 +53,20 @@ $totalPages = ceil($totalSets / $perPage);
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Overzicht</title>
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Speelhuys - Admin</title>
     <link rel="stylesheet" href="../overzicht.css">
+    <link rel="stylesheet" href="../style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
 <body>
-<header class="site-header">
-
-    <div class="site-logo">
-        <h2>Speelhuys</h2>
+<nav class="navbar navbar-expand-lg nav-bar" data-bs-theme="light">
+    <div class="container-fluid">
+        <span class="navbar-brand">Speelhuys </br> codeblokken</span>
     </div>
-</header>    
+</nav> 
 
 <div class="page-layout">
 
--- admin/sets.php
+++ admin/sets.php
@@ -11,25 +11,21 @@ $sets = Sets::AlleSets(0, 100);
 <html lang="en">
 
 <head>
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Sets - Speelhuys</title>
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Speelhuys - Sets</title>
     <link rel="stylesheet" href="../overzicht.css">
-
+    <link rel="stylesheet" href="../style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
 <body>
 
-<header class="site-header">
-
-    <div class="site-logo">
-        <h2>Speelhuys</h2>
+<nav class="navbar navbar-expand-lg nav-bar" data-bs-theme="light">
+    <div class="container-fluid">
+        <span class="navbar-brand">Speelhuys </br> codeblokken</span>
     </div>
-
-</header>
+</nav> 
 
 
 <div class="page-layout">
